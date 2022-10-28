b6441b335886512256d7d38a70eff22a50e72748
TiyaZhan <tizhan@ucsd.edu>
2022-10-27 20:59:11 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,44 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  //const synth = SpeechSynthesis;
+  let voices = []
+  const voiceSelect = document.querySelector('select');
+
+  function setUpVoice(){
+    voices = window.speechSynthesis.getVoices();
+    for (let i = 0; i < voices.length;i ++)
+    {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name}(${voices[i].lang})`;
+      option.setAttribute('name', voices[i].name);
+      option.setAttribute('lang', voices[i].lang);
+      voiceSelect.appendChild(option);
+    }
+  }
+  setUpVoice();
+  speechSynthesis.onvoiceschanged = setUpVoice;
+  
+  
+  const speak = document.querySelector('button');
+
+  speak.addEventListener('click', (event)=>{
+    const choice = voiceSelect.selectedOptions[0].getAttribute('name');
+    const audio = document.createElement('audio');
+    const txt = document.getElementById('text-to-speak');
+    const utter = new SpeechSynthesisUtterance(txt.value);
+    for (let i = 0; i < voices.length; i++)
+    {
+      if (voices[i].name === choice)
+      {
+        utter.voice = voices[i];
+      }
+    }
+    window.speechSynthesis.speak(utter);
+    
+  })
+  
+
 }
+
+console.log(document.getElementById('text-to-speak').value);
